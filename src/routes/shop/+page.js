3c62db4255483e622a5b2e68bb1ba8943dbbbf5e
cooperoscarfono <cooper@oscarfono.com>
2024-03-22 15:57:30 +1300

    changed store prerender status for now

--- src/routes/shop/+page.js
+++ src/routes/shop/+page.js
@@ -6,7 +6,7 @@ export const csr = dev;
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
 
 export const load = async ({ fetch }) => {
   const response = await fetch(`/api/products`);
